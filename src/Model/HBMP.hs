15270b98ebe438ddaf0f279292b406c7374dd90e
Toshiaki Honda <frenzieddoll@gmail.com>
2026-05-02 01:03:18 +0900

    del 不要になったファイルを削除

--- src/Model/HBMP.hs
+++
@@ -1,7 +0,0 @@
--- | このモジュールは削除されました。'Model.HBM' を使用してください。
-{-# OPTIONS_GHC -Wno-deprecations -Wno-missing-export-lists #-}
-module Model.HBMP {-# DEPRECATED "Model.HBMP は Model.HBM に統合されました。import Model.HBM に置き換えてください。" #-}
-  ( module Model.HBM
-  ) where
-
-import Model.HBM
